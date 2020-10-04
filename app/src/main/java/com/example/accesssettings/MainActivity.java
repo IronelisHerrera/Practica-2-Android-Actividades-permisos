46b5cd917a46023b3160fbb64e7df6a41d8fd3a5
IronelisHerrera <40542187+IronelisHerrera@users.noreply.github.com>
2020-10-04 16:40:44 -0400

    cancel btn is working just fine

--- app/src/main/java/com/example/accesssettings/MainActivity.java
+++ app/src/main/java/com/example/accesssettings/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.accesssettings;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    Button cancel_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //close the application
+        cancel_btn = findViewById(R.id.button2);
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAndRemoveTask();
+            }
+        });
+
     }
+
+    //Call when clicked continue botton
+
+
+
 }
