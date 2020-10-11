58fdbbe79b42dffd95c1813b8cd8b71b7002b9d8
IronelisHerrera <40542187+IronelisHerrera@users.noreply.github.com>
2020-10-11 02:11:32 -0400

    first permission revision, second activity settings and design

--- app/src/main/java/com/example/accesssettings/MainActivity.java
+++ app/src/main/java/com/example/accesssettings/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.accesssettings;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.*;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Button cancel_btn;
+    Button continue_btn;
+    Switch storage_switch_btn;
+    Switch location_switch_btn;
+    Switch camara_switch_btn;
+    List<String> request_permissions_array = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
 
         //close the application
         cancel_btn = findViewById(R.id.button2);
+
+        //Search switch ids componentents
+        storage_switch_btn = findViewById(R.id.switch6);
+        location_switch_btn = findViewById(R.id.switch7);
+        camara_switch_btn = findViewById(R.id.switch8);
+        continue_btn = findViewById(R.id.button_continue);
+
+
         cancel_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,9 +49,64 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        //Activities workflow - permissions
+
+        check_storage_already_granted_permission();
+        check_location_already_granted_permission();
+        check_camara_already_granted_permission();
+
+
     }
 
-    //Call when clicked continue botton
+    //check for already granted permissions
+
+    private Boolean check_storage_already_granted_permission(){
+
+     if(ContextCompat.checkSelfPermission(MainActivity.this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+         storage_switch_btn.setChecked(true);
+     }
+        return false;
+    }
+    private Boolean check_location_already_granted_permission(){
+        if(ContextCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+          location_switch_btn.setChecked(true);
+        }
+        return false;
+    }
+    private Boolean check_camara_already_granted_permission(){
+        if(ContextCompat.checkSelfPermission(MainActivity.this, CAMERA) == PackageManager.PERMISSION_GRANTED){
+            camara_switch_btn.setChecked(true);
+        }
+        return false;
+    }
+
+
+    //Call when clicked continue button
+    public void when_clicked_continue_btn(View btn){
+       Intent continue_btn_intent = new Intent(this, given_permission.class);
+       startActivity(continue_btn_intent);
+
+    }
+
+
+
+    private String request_missing_permissins(){
+
+                if(!check_storage_already_granted_permission() ){
+                    request_permissions_array.add(READ_EXTERNAL_STORAGE);
+                }
+                if(!check_location_already_granted_permission()){
+                    request_permissions_array.add(ACCESS_FINE_LOCATION);
+                }
+                if(!check_camara_already_granted_permission()){
+                    request_permissions_array.add(CAMERA);
+                }
+
+        return "";
+
+    }
+
 
 
 
---
+++ app/src/main/java/com/example/accesssettings/given_permission.java
@@ -0,0 +1,17 @@
+package com.example.accesssettings;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class given_permission extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_given_permission);
+
+        Intent get_continue_btn_intent = new Intent();
+
+    }
+}
