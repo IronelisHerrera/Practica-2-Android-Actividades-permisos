d2579ab918e164f7cca9d8091e55020a4643208e
IronelisHerrera <40542187+IronelisHerrera@users.noreply.github.com>
2020-10-13 04:57:44 -0400

    Correcting a few errors

--- app/src/main/java/com/example/accesssettings/MainActivity.java
+++ app/src/main/java/com/example/accesssettings/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +25,10 @@ public class MainActivity extends AppCompatActivity {
     Switch storage_switch_btn;
     Switch location_switch_btn;
     Switch camara_switch_btn;
-    Switch phone_witch_btn;
-    Switch contacts_witch_btn;
+    Switch phone_switch_btn;
+    Switch contacts_switch_btn;
     List<String> request_permissions_array = new ArrayList<>();
-    public static final String NOT_GRANTED_PERMISSION = "notGranted";
+    public static final int CODE_REQUEST_PERMISSION = 200;
 
 
     @Override
@@ -40,8 +42,8 @@ public class MainActivity extends AppCompatActivity {
         storage_switch_btn = findViewById(R.id.switch6);
         location_switch_btn = findViewById(R.id.switch7);
         camara_switch_btn = findViewById(R.id.switch8);
-        phone_witch_btn = findViewById(R.id.switch9);
-        contacts_witch_btn = findViewById(R.id.switch10);
+        phone_switch_btn = findViewById(R.id.switch9);
+        contacts_switch_btn = findViewById(R.id.switch10);
         continue_btn = findViewById(R.id.button_continue);
 
         //close the application
@@ -52,18 +54,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Activities workflow - permissions
         check_already_granted_permission();
-
     }
-
-    //check for already granted permissions
 
     private Boolean check_already_granted_permission(){
 
          if(ContextCompat.checkSelfPermission(MainActivity.this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
              storage_switch_btn.setChecked(true);
              storage_switch_btn.setClickable(false);
+
 
          }
          if(ContextCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
@@ -77,48 +76,75 @@ public class MainActivity extends AppCompatActivity {
 
          }
          if(ContextCompat.checkSelfPermission(MainActivity.this, CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
-             phone_witch_btn.setChecked(true);
-             phone_witch_btn.setClickable(false);
+             phone_switch_btn.setChecked(true);
+             phone_switch_btn.setClickable(false);
+
 
          }
          if(ContextCompat.checkSelfPermission(MainActivity.this, READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
-             contacts_witch_btn.setChecked(true);
-             contacts_witch_btn.setClickable(false);
-
+             contacts_switch_btn.setChecked(true);
+             contacts_switch_btn.setClickable(false);
          }
         return false;
     }
 
-    //Call when clicked continue button
-    public void when_clicked_continue_btn(View btn){
-           Intent continue_btn_intent = new Intent(this, given_permission.class);
+    public void when_clicked_continue_btn(View continue_btn){
 
-           if(!storage_switch_btn.isChecked()){
-               request_permissions_array.add(READ_EXTERNAL_STORAGE);
-           }
-           if(!location_switch_btn.isChecked()){
-               request_permissions_array.add(ACCESS_FINE_LOCATION);
-           }
-           if(!camara_switch_btn.isChecked()){
-               request_permissions_array.add(CAMERA);
-           }
-           if(!phone_witch_btn.isChecked()){
-               request_permissions_array.add(CALL_PHONE);
-           }
-           if(!contacts_witch_btn.isChecked()){
-               request_permissions_array.add(READ_CONTACTS);
-           }
-           if(!request_permissions_array.isEmpty()){
-               ActivityCompat.requestPermissions(this, new String[]{request_permissions_array.toString()}, 200);
+        Intent continue_btn_intent = new Intent(continue_btn.getContext(), given_permission.class);
+        if(storage_switch_btn.isChecked() ){
+            if(ContextCompat.checkSelfPermission(MainActivity.this, READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                request_permissions_array.add(READ_EXTERNAL_STORAGE);
+            }
+        }
+        if(location_switch_btn.isChecked() ){
+            if(ContextCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                request_permissions_array.add(ACCESS_FINE_LOCATION);
+            }
+        }
+        if(camara_switch_btn.isChecked() ){
+            if(ContextCompat.checkSelfPermission(MainActivity.this, CAMERA) != PackageManager.PERMISSION_GRANTED){
+                request_permissions_array.add(CAMERA);
+            }
+        }
+        if(phone_switch_btn.isChecked() ){
+            if(ContextCompat.checkSelfPermission(MainActivity.this, CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                request_permissions_array.add(CALL_PHONE);
+            }
+        }
+        if(contacts_switch_btn.isChecked() ){
+            if(ContextCompat.checkSelfPermission(MainActivity.this, READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+                request_permissions_array.add(READ_CONTACTS);
+            }
+        }
+        if(!request_permissions_array.isEmpty()){
+            ActivityCompat.requestPermissions(MainActivity.this, request_permissions_array.toArray(new String[0]), CODE_REQUEST_PERMISSION);
 
-           }
-
-
-           startActivity(continue_btn_intent);
+        }else {
+            //Intent continue_btn_intent = new Intent(getApplicationContext(), given_permission.class);
+            startActivity(continue_btn_intent);
+        }
+        //startActivity(continue_btn_intent_first);
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
 
+        if(requestCode == CODE_REQUEST_PERMISSION ){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Snackbar.make(contacts_switch_btn, "Permiso otorgado", Snackbar.LENGTH_SHORT).show();
+                Intent continue_btn_intent = new Intent(getApplicationContext(), given_permission.class);
+                startActivity(continue_btn_intent);
+
+            } else {
+                Snackbar.make(contacts_switch_btn, "El permiso no fue otorgado", Snackbar.LENGTH_SHORT).show();
+            }
+
+        }else{
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
 
 
 }
--- app/src/main/java/com/example/accesssettings/given_permission.java
+++ app/src/main/java/com/example/accesssettings/given_permission.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,7 +14,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,28 +22,27 @@ import com.google.android.material.snackbar.Snackbar;
 public class given_permission extends AppCompatActivity {
 
 
-    Button storage;
-    Button location;
-    Button call_phone;
-    Button check_contact;
-    ImageView android_image;
-    ImageView storage_image;
+   private Button storage;
+   private Button location;
+   private Button call_phone;
+   Button check_contact;
+   private ImageView android_image;
+   ImageView storage_image;
 
     final static int LOCATION_CODE = 201;
     final static int CAMARA_CODE = 202;
-    private static final int PHONE_CALL_NUMBER = 203;
     final static int CONTACTS_CODE = 204;
     final static int STORAGE_CODE = 205;
     final static int PHONE_CALL_CODE = 206;
     final static String GOOGLE_MAPS_URL = "https://www.google.com/maps/place/Pontificia+Universidad+Cat%C3%B3lica+Madre+y+Maestra/@19.4436055,-70.6865672,17z/data=!3m1!4b1!4m5!3m4!1s0x8eb1cf64929050fb:0x4f33915a6b4a939a!8m2!3d19.4436005!4d-70.6843785";
+    private static final String number_to_call ="84000000";
 
-    //private static final String NUMBER = "tel:8400000";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_given_permission);
-
-        Intent get_continue_btn_intent = getIntent();
+        //Intent get_continue_btn_intent = getIntent();
         storage = findViewById(R.id.storage_btn_id);
         location = findViewById(R.id.location_btn_id);
         storage_image = findViewById(R.id.imageView_storage);
@@ -53,16 +50,14 @@ public class given_permission extends AppCompatActivity {
         call_phone = findViewById(R.id.phone_btn_id);
         check_contact = findViewById(R.id.contacts_btn_id);
 
-
-
-
     }
+
     public void check_storage_listener(View storage_btn){
         if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
 
             AlertDialog.Builder window_storage_dialog = new AlertDialog.Builder(this);
-            window_storage_dialog.setMessage("Quiere acceder al almacenamiento?");
             window_storage_dialog.setTitle("CUADRO INFORMATIVO");
+            window_storage_dialog.setMessage("¿Quiere acceder al almacenamiento?");
             window_storage_dialog.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
@@ -126,15 +121,20 @@ public class given_permission extends AppCompatActivity {
         Uri location_uri = Uri.parse(GOOGLE_MAPS_URL);
         Intent location_intent= new Intent(Intent.ACTION_VIEW, location_uri);
         location_intent.setPackage("com.google.android.apps.maps");
-        startActivityForResult(location_intent, LOCATION_CODE);
-    }
+        if(location_intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(location_intent, LOCATION_CODE);
+        }
+        else{
+            Toast.makeText(this, "No se pudo acceder a la hubicación", Toast.LENGTH_SHORT).show();
+        }
 
+    }
 
     public void check_camara_listener(View camara_btn){
         if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
             AlertDialog.Builder window_camara_dialog = new AlertDialog.Builder(this);
-            window_camara_dialog.setMessage("Quiere acceder a la cámara?");
             window_camara_dialog.setTitle("CUADRO INFORMATIVO");
+            window_camara_dialog.setMessage("¿Quiere acceder a la cámara?");
             window_camara_dialog.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
@@ -160,27 +160,14 @@ public class given_permission extends AppCompatActivity {
     }
 
     private void access_to_my_camara() {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-         startActivityForResult(cameraIntent, CAMARA_CODE);
+        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(camera_intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(camera_intent, CAMARA_CODE);
+        }else {
+            Toast.makeText(this, "No pudo accederse a la camara", Toast.LENGTH_SHORT).show();
+        }
     }
-    //phone
-    public void call_phone_listener(View location_btn){
-//        if(checkSelfPermission(Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_DENIED){
-//
-//            AlertDialog.Builder window_phone_dialog = new AlertDialog.Builder(this);
-//            window_phone_dialog.setTitle("CUADRO INFORMATIVO");
-//            window_phone_dialog.setMessage("Para realizar esta acción debe otorgar permiso para acceder a los contactos");
-//            window_phone_dialog.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    dialog.dismiss();
-//
-//                }
-//            });
-//            window_phone_dialog.create().show();
-//
-//        }
-        //else{
+    public void call_phone_listener(View phone_btn){
 
             if(checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
                 AlertDialog.Builder window_phone_dialog = new AlertDialog.Builder(this);
@@ -207,14 +194,14 @@ public class given_permission extends AppCompatActivity {
                 }
             }
             requestPermissions(new String[] {Manifest.permission.CALL_PHONE}, 200);
-        //}
 
     }
-    public void check_contacts_listener(View contacts_btn){
+
+    public void check_contacts_listener(View v){
         if(checkSelfPermission(Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
             AlertDialog.Builder window_contacts_dialog = new AlertDialog.Builder(this);
             window_contacts_dialog.setTitle("CUADRO INFORMATIVO");
-            window_contacts_dialog.setMessage("Si acepta esta opción, estará otorgando permiso para que la aplicación pueda la información de contactos");
+            window_contacts_dialog.setMessage("¿Quiere acceder a los contactos?");
             window_contacts_dialog.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
@@ -232,58 +219,34 @@ public class given_permission extends AppCompatActivity {
         }
         else{
             if(shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)){
-                Snackbar.make(check_contact, "Es necesario que solicite permiso para acceder", Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(android_image, "Es necesario que solicite permiso para acceder", Snackbar.LENGTH_SHORT).show();
             }
         }
-        requestPermissions(new String[] {Manifest.permission.CAMERA}, 200);
-
+        requestPermissions(new String[] {Manifest.permission.READ_CONTACTS}, 200);
     }
+
 
     private void check_contacts() {
-
         Intent check_contacts = new Intent(Intent.ACTION_PICK);
+        //Intent check_contacts = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
         check_contacts.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
-
-        if (check_contacts.resolveActivity(getPackageManager()) != null) {
+        if(check_contacts.resolveActivity(getPackageManager()) != null){
             startActivityForResult(check_contacts, CONTACTS_CODE);
-
         }else{
-            Toast.makeText(this, "Ocurrió un error al entrar a la agenda", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Hubo un error al tratar de acceder a los contactos", Toast.LENGTH_SHORT).show();
         }
-    }
 
+    }
     private void call_contact_number() {
-
-        // Start an activity for the user to pick a phone number from contacts
-        if(checkSelfPermission(Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_DENIED){
-            Intent intent = new Intent(Intent.ACTION_PICK);
-            intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(intent, PHONE_CALL_CODE);
-            }
-            else {
-                Toast.makeText(this, "No se pudo llamar!!!", Toast.LENGTH_SHORT).show();
-            }
-        }else{
-            Toast.makeText(this, "Para realizar esta acción debe otorgar permiso para acceder a los contactos", Toast.LENGTH_SHORT).show();
-        }
-
-
-
-//        Intent call_phone_number = new Intent(Intent.ACTION_CALL);
-//        call_phone_number.setData(Uri.parse(NUMBER));
-//        if (call_phone_number.resolveActivity(getPackageManager()) != null) {
-//            startActivityForResult(call_phone_number, PHONE_CALL_CODE);
-//
-//        }else{
-//            Toast.makeText(this, "No se pudo llamar al número"+" "+NUMBER, Toast.LENGTH_SHORT).show();
-//        }
-
+         Intent intent = new Intent(Intent.ACTION_CALL);
+         intent.setData(Uri.parse("tel:"+number_to_call));
+         if (intent.resolveActivity(getPackageManager()) != null) {
+             startActivityForResult(intent, PHONE_CALL_CODE);
+         }
+         else {
+             Toast.makeText(this, "No se pudo llamar al número"+" "+number_to_call, Toast.LENGTH_SHORT).show();
+         }
     }
-
-
-
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
@@ -308,19 +271,7 @@ public class given_permission extends AppCompatActivity {
 
         if ( resultCode == RESULT_OK) {
             if(requestCode == PHONE_CALL_CODE){
-                // Get the URI and query the content provider for the phone number
-                Intent c = new Intent(Intent.ACTION_CALL);
-                Uri contactUri = data.getData();
-                String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER};
-                Cursor cursor = getContentResolver().query(contactUri, projection,
-                        null, null, null);
-                // If the cursor returned is valid, get the phone number
-                if (cursor != null && cursor.moveToFirst()) {
-                    int numberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-                    String number = cursor.getString(numberIndex);
-                    c.setData(Uri.parse("tel:"+number));
-                    startActivityForResult(c, PHONE_CALL_CODE);
-                }
+                Toast.makeText(this, "OK!", Toast.LENGTH_SHORT).show();
             }
 
         }
@@ -328,37 +279,6 @@ public class given_permission extends AppCompatActivity {
             if(requestCode ==CONTACTS_CODE){
                 Toast.makeText(this, "OK!", Toast.LENGTH_SHORT).show();
             }
-        }
-
-
-//        if(resultCode == RESULT_OK) {
-//            if (requestCode == PHONE_CALL_CODE) {
-//                //
-//                // Get the URI and query the content provider for the phone number
-//                Toast.makeText(this, "OK!", Toast.LENGTH_SHORT).show();
-//            }
-//
-//        }
-
-
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
-
-
-        if(requestCode == 200 && grantResults.length >0 ){
-            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
-                Snackbar.make(storage, "Permiso otorgado", Snackbar.LENGTH_SHORT).show();
-
-            } else {
-                Snackbar.make(storage, "El permiso no fue otorgado", Snackbar.LENGTH_SHORT).show();
-            }
-
-
-        }else{
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
     }
